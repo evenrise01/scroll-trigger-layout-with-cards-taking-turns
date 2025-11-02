e27236eced65dba71a43a965e9550a5ca6cd8055
Daksh Singh <63799338+evenrise01@users.noreply.github.com>
2025-11-03 00:51:48 +0530

    Added assets and html structure for gsap animation

--- app/page.tsx
+++ app/page.tsx
@@ -2,64 +2,68 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <>
+      <section className="intro">
+        <h1>
+          We create modern website designs that feel effortlessly personal.
+        </h1>
+      </section>
+      <section className="sticky-cols">
+        <div className="sticky-cols-wrapper">
+          <div className="col col-1">
+            <div className="col-content">
+              <div className="col-content-wrapper">
+                <h1>We design websites where modern meets elevation.</h1>
+                <p>
+                  Layered textures, rich tones, and thoughtful details come
+                  together to create a website that feels elevated.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col col-2">
+            <div className="col-img col-img-1">
+              <div className="col-img-wrappe">
+                <img src="img-1.jpg" alt="" />
+              </div>
+            </div>
+            <div className="col-img col-img-2">
+              <div className="col-img-wrappe">
+                <img src="img-2.jpg" alt="" />
+              </div>
+            </div>
+          </div>
+          <div className="col col-3">
+            <div className="col-content-wrapper">
+              <h1>Our websites are crafted to feel as calm as they look.</h1>
+              <p>
+                Each website is designed with intentional balance between warmth
+                and modernity, light and shadow, function and beauty.
+              </p>
+            </div>
+            <div className="col-content-wrapper-2">
+              <h1>
+                Every detail is chosen to bring ease and elegance into your
+                website.
+              </h1>
+              <p>
+                From custom animations to illustrations, we shape websites that
+                reflect your brand with timeless clarity.
+              </p>
+            </div>
+          </div>
+          <div className="col col-4">
+            <div className="col-img">
+              <div className="col-img-wrapper">
+                <img src="img-3.jpg" alt="" />
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+      </section>
+      <section className="outro">
+        <h1>Timeless design begins with a conversation.</h1>
+      </section>
+    </>
   );
 }
